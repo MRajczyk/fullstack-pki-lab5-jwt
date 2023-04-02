911c939f2771e2da2769353da9eeabd23b2d17eb
MRajczyk <mikolajrajczyk01@gmail.com>
2023-04-02 20:09:26 +0200

    fixed unauthorized access to all users

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,12 +1,19 @@
-import { UserModel } from '../models/user.js' 
+import { UserModel } from '../models/user.js'
+import {decodeJwt} from "../util/utij.js";
 
 // Controller get users list 
-export const getUserList = async (req, res, next) => { 
+export const getUserList = async (req, res, next) => {
+    const reqUserRole = decodeJwt(req.headers.authorization.split(" ")[1]).rol;
+    if(reqUserRole !== "ADMIN") {
+        res.status(401).send({message: "Unauthorized"});
+        next();
+        return;
+    }
     const users = await UserModel.find({})
     //console.log(users)
     if (!users) { 
         res.status(401).send({message: "Unauthorized"}) 
-        next(err) 
+        next()
     } else { 
         res.json({status: "success", users: users}); 
     } 
