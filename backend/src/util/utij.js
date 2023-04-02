740a68ce716dcbf3a432126ea05f6a72429e8c60
MRajczyk <mikolajrajczyk01@gmail.com>
2023-04-02 20:09:41 +0200

    var => const in util.js

--- backend/src/util/utij.js
+++ backend/src/util/utij.js
@@ -1,5 +1,5 @@
 export function decodeJwt(token) {
-    var base64Payload = token.split(".")[1];
-    var payloadBuffer = Buffer.from(base64Payload, "base64");
+    const base64Payload = token.split(".")[1];
+    const payloadBuffer = Buffer.from(base64Payload, "base64");
     return JSON.parse(payloadBuffer.toString());
 }
