4e7dd9621f8937eafaeb4bd1a9c5079483e91e6f
MRajczyk <mikolajrajczyk01@gmail.com>
2023-04-02 20:13:02 +0200

    added endpoints for data dependant on roles

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,20 +1,20 @@
 import express from 'express' 
 import { loginUser, refreshTokenVerify, accessTokenVerify, createUser} from './controllers/auth.js' 
-import { getDataList } from './controllers/data.js';
+import { getPublicData, getLoggedUserData, getAdminData } from './controllers/data.js';
 import { getUserList } from './controllers/users.js'
 
 const router = express.Router(); 
 
 router.post('/auth/signin', loginUser); 
-router.post('/auth/register', createUser); 
+router.post('/auth/register', createUser);
 router.post('/refresh', refreshTokenVerify); 
-//
-router.get('/data/public', getDataList);
 
 // secure router 
 router.get('/users', accessTokenVerify, getUserList);
-//
-router.get('/data/logged', accessTokenVerify, getDataList);
-router.get('/data/admin', accessTokenVerify, getDataList);
+
+//data endpoints
+router.get('/data/public', getPublicData);
+router.get('/data/logged', accessTokenVerify, getLoggedUserData);
+router.get('/data/admin', accessTokenVerify, getAdminData);
 
 export default router;
