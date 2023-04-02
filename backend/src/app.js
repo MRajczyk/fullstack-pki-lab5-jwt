003451cadc587ad20181536f85fb72ae10b5fef6
MRajczyk <mikolajrajczyk01@gmail.com>
2023-04-02 20:13:38 +0200

    added errorhandling with db init

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,18 +7,15 @@ import { config } from '../config.js';
 import { initializeUser } from './seed/user-seeder.js'
 import { initializeData } from './seed/data-seeder.js'
 
- 
 // Initialize app 
 const app = express(); 
-
 
 app.use(cors()); 
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({extended: false})); 
-app.get('/', (req, res) => { 
+app.get('/', (req, res) => {
   res.json({app: 'Run app auth'}); 
 }); 
-
 
 // Connect to MongoDB 
 mongoose.connect(config.URI_MONGO, { 
@@ -27,18 +24,26 @@ mongoose.connect(config.URI_MONGO, {
 
  
 mongoose.connection.on('connected', () => { 
-  initializeUser() 
-  initializeData() 
-  console.log('Initialize user') 
+  initializeUser().then((err) => {
+    if(err) {
+      console.log(err.message);
+    }
+  });
+
+  initializeData().then((err) => {
+    if(err) {
+      console.log(err.message);
+    }
+  });
+  console.log('Initialized user and data')
 }); 
 mongoose.connection.on('error', (err) => { 
   console.log('Error: Could not connect to MongoDB.', err); 
 }); 
 
- 
 // Routes app 
 app.use('/', routes); 
 // Start app 
 app.listen(config.PORT_LISTEN, () => { 
-  console.log('Listen port ' + config.PORT_LISTEN); 
+  console.log('Listening at port ' + config.PORT_LISTEN);
 })
