e57ea0ce00875cffbccee904ef02f145db487c81
MRajczyk <mikolajrajczyk01@gmail.com>
2023-04-02 20:15:16 +0200

    data endpoints conn with database implemented and inquirer role validated

--- backend/src/controllers/data.js
+++ backend/src/controllers/data.js
@@ -1,15 +1,50 @@
 import { DataModel } from '../models/data.js' 
 import { decodeJwt } from '../util/utij.js'
 
-// Controller get users list 
-export const getDataList = async (req, res, next) => { 
-    //console.log(decodeJwt(req.headers.authorization.split(" ")[1]).rol)
-    const data = await DataModel.find({requiredRole: decodeJwt(req.headers.authorization.split(" ")[1]).rol})
+export const getPublicData = async (req, res, next) => {
+    const data = await DataModel.find({$or:[{requiredRole: ""}]});
+
     //console.log(data)
-    if (!data) { 
-        res.status(401).send({message: "Unauthorized"}) 
-        next(err) 
-    } else { 
-        res.json({status: "success", data: data}); 
-    } 
+    if (!data) {
+        res.status(401).send({message: "Unauthorized"})
+        next()
+    } else {
+        res.json({status: "success", data: data});
+    }
+}
+
+export const getLoggedUserData = async (req, res, next) => {
+    const reqUserRole = decodeJwt(req.headers.authorization.split(" ")[1]).rol;
+    if(reqUserRole !== "USER" && reqUserRole !== "ADMIN") {
+        res.status(401).send({message: "Unauthorized"});
+        next();
+        return;
+    }
+
+    const data = await DataModel.find({$or:[{requiredRole: ""}, {requiredRole: 'USER'}]});
+    //console.log(data)
+    if (!data) {
+        res.status(401).send({message: "Unauthorized"});
+        next();
+    } else {
+        res.json({status: "success", data: data});
+    }
+}
+
+export const getAdminData = async (req, res, next) => {
+    const reqUserRole = decodeJwt(req.headers.authorization.split(" ")[1]).rol;
+    if(reqUserRole !== "ADMIN") {
+        res.status(401).send({message: "Unauthorized"});
+        next();
+        return;
+    }
+
+    const data = await DataModel.find({$or:[{requiredRole: ""}, {requiredRole: 'USER'}, {requiredRole: 'ADMIN'}]});
+    //console.log(data)
+    if (!data) {
+        res.status(401).send({message: "Unauthorized"});
+        next();
+    } else {
+        res.json({status: "success", data: data});
+    }
 }
