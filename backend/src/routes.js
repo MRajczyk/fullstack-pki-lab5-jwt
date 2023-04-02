5fe8b383fb945b3dd78319109ee04a198919eba2
MRajczyk <mikolajrajczyk01@gmail.com>
2023-04-02 04:21:18 +0200

    added new data routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 import express from 'express' 
 import { loginUser, refreshTokenVerify, accessTokenVerify, createUser} from './controllers/auth.js' 
+import { getDataList } from './controllers/data.js';
 import { getUserList } from './controllers/users.js'
 
 const router = express.Router(); 
@@ -7,12 +8,13 @@ const router = express.Router();
 router.post('/auth/signin', loginUser); 
 router.post('/auth/register', createUser); 
 router.post('/refresh', refreshTokenVerify); 
-
-router.get('/data/public', loginUser); 
-router.get('/data/logged', loginUser); 
-router.get('/data/admin', loginUser); 
+//
+router.get('/data/public', getDataList);
 
 // secure router 
 router.get('/users', accessTokenVerify, getUserList);
+//
+router.get('/data/logged', accessTokenVerify, getDataList);
+router.get('/data/admin', accessTokenVerify, getDataList);
 
 export default router;
